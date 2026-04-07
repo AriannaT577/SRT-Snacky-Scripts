b1e2f958c65a033b712c6a02632673a509fa6071
f <119464647+AriannaT577@users.noreply.github.com>
2026-04-06 23:07:52 -0600

    Add files via upload

---
+++ FTC/INTO THE DEEP 2024-2025/Other/LifeOnHardMode.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class LifeOnHardMode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor FL = hardwareMap.dcMotor.get("FL");
+        DcMotor BL = hardwareMap.dcMotor.get("BL");
+        DcMotor FR = hardwareMap.dcMotor.get("FRfr");
+        DcMotor BR = hardwareMap.dcMotor.get("BRuh");
+        
+        FR.setDirection(DcMotorSimple.Direction.REVERSE);
+        BR.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double FLPower = (y + x + rx) / denominator;
+            double BLPower = (y - x + rx) / denominator;
+            double FRPower = (y - x - rx) / denominator;
+            double BRPower = (y + x - rx) / denominator;
+
+            FL.setPower(FLPower);
+            BL.setPower(BLPower);
+            FR.setPower(FRPower);
+            BR.setPower(BRPower);
+        }
+    }
+}
