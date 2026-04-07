0075dd1d75ab6241754317b8835aa4e405729e38
f <119464647+AriannaT577@users.noreply.github.com>
2026-04-06 22:56:14 -0600

    Create 1-helloworld.py

---
+++ Computer Science/Structured Programming/1-helloworld.py
@@ -0,0 +1,4 @@
+print ("Hello World")
+print ("My name is Arianna Tay")
+print ("I am in Grade 10")
+print ("Currently, I am taking Computer Science to learn and develop the skills to code and use technology effectively")
