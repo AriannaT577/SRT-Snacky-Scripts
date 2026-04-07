e8c70524704fe3b3125366d4345898d9458ed309
f <119464647+AriannaT577@users.noreply.github.com>
2026-04-06 22:57:06 -0600

    Add files via upload

---
+++ Computer Science/Structured Programming/2-strings.py
@@ -0,0 +1,22 @@
+#strings
+
+print("Text is stored as a data type called a STRING")
+print("To classify text as a string you put in quotation marks")
+print('You can even use single quotations if you prefer')
+#print(What do you think this hashtag does?")
+print ('This won\'t print, ERROR!')
+
+print ("\nThis is the first line \nand this is on a newline")
+print ("\tThis text is indendeted using TAB")
+
+#logo
+print("         /\\")
+print("        // \\")
+print("       //___\\")
+print("      //-----\\             __________")
+print("     //       \\            ----||----")
+print("    /          \         ---   ||")
+print("   /         ___\_____---      ||")
+print("  /                            ||")
+print(" /       o                     ||")
+print("/             \'     o          || ")
