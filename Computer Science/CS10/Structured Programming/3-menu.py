1f4c74d61fbade86c9581cc7de041b3555620e3b
f <119464647+AriannaT577@users.noreply.github.com>
2026-04-06 22:49:39 -0600

    Add files via upload

---
+++ Computer Science/CS10/Structured Programming/3-menu.py
@@ -0,0 +1,32 @@
+#5+ menu items, 1+ comment in code, 1+ escape character, and alliterations (yipee)
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~***~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("|                Festive Foods and Fruits Menu                |")
+print("|\t     ************************************             |\n|                                                             |")
+#drinks that are fruit based yum yum
+print("|  DRINKS                                                     |")
+print("|  Sweet Strawberry Smoothies............................ $36 |")
+print("|  Joyful Jackfruit Juice................................ $38 |")
+print("|  Remarkable Rasberry Refreshers........................ $35 |")
+
+#foods that are alliteration cuz yum yum
+print("|                                                             |\n|                                                             |")
+print("|  FOODS                                                      |")
+print("|  Spicy Salmon Sandwich................................. $42 |")
+print("|  Spinach Salad......................................... $46 |")
+print("|  Satisfying Sausage Soup............................... $45 |\n|                                                             |")
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~***~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+#logo
+print("         /\\")
+print("        // \\")
+print("       //___\\")
+print("      //-----\\             __________")
+print("     //       \\            ----||----")
+print("    /          \         ---   ||")
+print("   /         ___\_____---      ||")
+print("  /                            ||")
+print(" /       o                     ||")
+print("/             \'     o          || ")
+
---
+++ Computer Science/CS10/Structured Programming/8-variables.py
@@ -0,0 +1,32 @@
+#create 2 more variables, 3 more str of code w variables
+apples = 100
+apples_per_bag = 8
+total_amount_of_money = 420
+cost_of_bag = 69
+bags2buy = 6
+
+
+print("Number of apples: "+str(apples))
+print("Number of apples in bag: "+str(apples_per_bag))
+print("Number of full bags: "+str(apples//apples_per_bag))
+print("Number of apples left over: "+str(apples % apples_per_bag))
+
+print("Number of bags of apples to buy: "+str (bags2buy))#3
+print("Cost of bags of apples: "+str(cost_of_bag))#69
+print("Total cost of the bags of apples: "+str(bags2buy*cost_of_bag))#207
+print("Remaining Money: "+str(total_amount_of_money-(bags2buy*cost_of_bag)))#213
+
+#logo
+
+#logo
+print("         /\\\\\
+\n        // \\\\\
+\n       //___\\\\\
+\n      //-----\\\\             __________\
+\n     //       \\\            ----||----\
+\n    /          \\         ---    ||\
+\n   /         ___\\_____---       ||\
+\n  /                             ||\
+\n /       o                      ||\
+\n/             \'     o           || ")
+
